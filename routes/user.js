74159abd744c148ef8a10894efc6dd7ce04aacd3
DanielSaed <84155356+DanielSaed@users.noreply.github.com>
2021-06-10 20:07:21 -0500

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const user = express.Router();
 const db = require('../config/database');
-var i == 0;
+var int i == 0;
 
 user.post("/signin",async(req,res,next) => {
 const {user_name,user_mail,user_password} = req.body;
